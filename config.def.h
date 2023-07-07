7fc98ad41f5f2d0a80cd7d0406bffee88365b5cc
usernyan <74884883+usernyan@users.noreply.github.com>
2023-07-07 07:15:41 -0400

    applied font2 patch

--- config.def.h
+++ config.def.h
@@ -5,7 +5,13 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=16:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+    "Fira Code:pixelsize=16:antialias=true:autohint=true",
+    "NotoColorEmoji:pixelsize=14:antialias=true:autohint=true",
+};
+
 static int borderpx = 2;
 
 /*
